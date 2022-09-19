ca6e1bfef796bb0201ed92388cac545802908c2f
Utro-tvar <utrotime@gmail.com>
2022-09-19 19:17:12 +0300

    Python script created.

---
+++ code/Python/ghoul.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def main():
+    print("I'm ghoul!!!")
+    n = 1000
+    for i in range(n, 6, -7):
+        print(f"{i} - 7 = {i - 7}")
+
+if __name__ == "__main__":
+    main()
